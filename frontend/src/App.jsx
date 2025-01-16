b600bacf2402acd51293e45b3bfb652c11c0925c
Anurag Pardeshi <pardeshianurag22@gmail.com>
2025-01-16 21:01:37 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+      <h1 className="text-6xl font-bold text-center">
+      TEN Agentic Workflow
+    </h1>
+  );
+}
+
+export default App;
