82942aa6a6a0463d884fe8299140d00992db404d
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2025-01-31 18:08:35 +0530

    deploy instructions added and ui changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Landing from "./components/Landing.jsx";
 import Home from "./components/Home.jsx";
 import CodeEditor from "./components/CodeEditor.jsx";
 import AIHome from "./pages/AIHome.jsx";
+import Deploy from "./components/Deploy.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/editor" element={<CodeEditor />} />
       {/* <Route path="/landing" element={<Landing />} /> */}
       <Route path="/ai" element={<AIHome />} />
+      <Route path="/deploy" element={<Deploy />} />
     </Routes>
   );
 }
