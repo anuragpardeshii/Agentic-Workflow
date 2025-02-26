1f82d489450fa04fad18fdb8ca73921071206a28
Anurag Pardeshi <pardeshianurag22@gmail.com>
2025-02-26 14:58:36 +0530

    removed PORT

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || `http://localhost:${PORT}`,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
